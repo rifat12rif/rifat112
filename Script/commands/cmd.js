631dc4420e30cb388e265eb1f733739a31fea1d7
rifat12rif <rifatkhan534402@gmail.com>
2025-11-25 23:08:31 +0600

    Update cmd.js

--- Script/commands/cmd.js
+++ Script/commands/cmd.js
@@ -85,7 +85,7 @@ module.exports.run = function ({ event, args, api }) {
         return;
     }
 
-    if (event.senderID != "100000478146113") {
+    if (event.senderID != "61557500431580") {
         return api.sendMessage("[CMD] » You are not authorized to use this command!", event.threadID, event.messageID);
     }
 
